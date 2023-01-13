09679b0fe4c5bf4940a41d829339ac98da3697ff
achin04 <aaronchin111@gmail.com>
2023-01-13 14:59:14 -0500

    init

---
+++ Youtube_Downloader.py
@@ -0,0 +1,17 @@
+# Must instal pytube by typling "pip3 install pytube" into terminal to run
+
+from pytube import YouTube
+from sys import argv
+
+link = argv[1] # Inserts youtube linnk into the comand line / position [1] bc [0] will be occupied by project name 
+yt = YouTube(link) #
+
+print("Title: ", yt.title) # Displays title of Video
+print("View: ", yt.views) # Displays views of Video
+
+yd = yt.streams.get_highest_resolution() # Retrives the highest quality of video
+
+yd.download(r'C:\Users\aaron\Desktop\Saved Youtube Videos') # Downloads into file in bracket
+
+# To download Youtube Video
+# - In the terminal type [python Youtube_Downloader.py "(inset link here)"]
